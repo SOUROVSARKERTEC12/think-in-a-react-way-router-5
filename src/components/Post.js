d33821c89df31da14a09388f74c0aed9274c1658
MD.SOUROVSARKAR <36516913+SOUROVSARKERTEC12@users.noreply.github.com>
2022-07-28 00:32:37 +0600

    Initial commit

---
+++ src/components/Post.js
@@ -0,0 +1,19 @@
+import { useNavigate,useParams } from "react-router-dom";
+export default function Post() {
+  const params = useParams();
+  const navigate = useNavigate();
+
+  function goBack(){
+      navigate("/posts",{
+          replace : true,
+      })
+  }
+
+  return (
+    <div>
+      <h1>Post</h1>
+      <p>Post id is {params.postId}</p>
+        <button type="button" onClick={goBack}>Go to Back</button>
+    </div>
+  );
+}
